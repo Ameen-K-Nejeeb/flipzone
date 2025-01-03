9600fb18cec966a709fdf1145afa6ee2e70bf006
DUDEamee <ameenameen8701@gmail.com>
2025-01-03 08:35:34 +0530

    update : the secret_key and removerd the desabled the repocitoryEnv

--- flipzone/settings.py
+++ flipzone/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Config, RepositoryEnv
+from decouple import Config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = Config(RepositoryEnv(BASE_DIR / '.env'))
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = Config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!

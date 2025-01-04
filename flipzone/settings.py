555171a5787dfc6068a7f975aa173c70e5ae8ffb
DUDEamee <ameenameen8701@gmail.com>
2025-01-04 23:21:42 +0530

    setuped

--- flipzone/settings.py
+++ flipzone/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+
 SECRET_KEY = Config('SECRET_KEY')
 
 

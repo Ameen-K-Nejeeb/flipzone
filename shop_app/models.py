685707be871a5a18900d4d218e5aeefe54783630
DUDEamee <ameenameen8701@gmail.com>
2025-01-06 20:48:28 +0530

    update : formated

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,30 +1,33 @@
 from django.db import models
 from django.utils.text import slugify
+
+
 # Create your models here.
 class Product(models.Model):
-    CATEGORY = (("Electronics", "ELECTRONICS"),
-                ("Groceries", "GROCERIES"),
-                ("Clothings", "CLOTHINGS")
-                )
-    
+    CATEGORY = (
+        ("Electronics", "ELECTRONICS"),
+        ("Groceries", "GROCERIES"),
+        ("Clothings", "CLOTHINGS"),
+    )
+
     name = models.CharField(max_length=100)
     slug = models.SlugField(blank=True, null=True)
     image = models.ImageField(upload_to="img")
     description = models.CharField(blank=True, null=True, max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=15 , choices=CATEGORY, blank=True, null=True)
+    category = models.CharField(max_length=15, choices=CATEGORY, blank=True, null=True)
 
-    def  __str__(self):
+    def __str__(self):
         return self.name
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
             unique_slug = self.slug
             counter - 1
             if Product.objects.filter(slug=unique_slug).exists():
-                unique_slug = f'{self.slug}-{counter}'
-                counter +=1
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
             self.slug = unique_slug
 
         super().save(*args, **kwargs)
